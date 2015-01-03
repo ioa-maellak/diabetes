800de10b780ce92fa6c36e6e4aea36021681373e
pmanousis <pmanousi@cs.uoi.gr>
2015-01-03 15:54:47 +0200

    Added a few sql related stuff.  Now we are able to use sqlite to store our data.  I, on purposed, added a few data to the database, just to check out.  Now what is pending is the forms that will help the user, as well as the other table that is for the nutrition stuff of the foods.

--- src/com/example/diabetes/MainActivity.java
+++ src/com/example/diabetes/MainActivity.java
@@ -1,16 +1,52 @@
 package com.example.diabetes;
 
 import android.app.Activity;
+import android.app.AlertDialog.Builder;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Context;
+import android.database.sqlite.*;
 
 public class MainActivity extends Activity {
 
+	SQLiteDatabase db;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// Creating database and tables
+		db=openOrCreateDatabase("diabetes", Context.MODE_PRIVATE, null);
+		
+		// TODO: remove thsi... used just for test purposes!
+		db.execSQL("DROP TABLE IF EXISTS BloodGlucose;");
+		db.execSQL("DROP TABLE IF EXISTS InsoulinDose;");
+		db.execSQL("DROP TABLE IF EXISTS InsoulinTypes;");
+		
+		db.execSQL("CREATE TABLE IF NOT EXISTS InsoulinTypes (insoulinID integer, name varchar(50), ActingStartMinutes integer, ActingPeakMinutes integer, durationMinutes integer, PRIMARY KEY (insoulinID));");
+		db.execSQL("CREATE TABLE IF NOT EXISTS InsoulinDose (givenAt TEXT DEFAULT CURRENT_DATETIME, insoulinType integer, dosage double precision, FOREIGN KEY (insoulinType) REFERENCES InsoulinTypes(insoulinID), PRIMARY KEY (givenAt, insoulinType) );");
+		db.execSQL("CREATE TABLE IF NOT EXISTS BloodGlucose (measuredAt TEXT DEFAULT CURRENT_DATETIME, glucoseValue smallint, PRIMARY KEY (measuredAt));");
+		
+		// TODO: remove this... used just for test purposes!
+		db.execSQL("INSERT INTO InsoulinTypes VALUES (1,'Novo Novorapid', 15, 67, 240);");	// Actually, keep this, it has real data.
+		db.execSQL("INSERT INTO InsoulinDose (insoulinType, dosage) VALUES (1, 14);");
+		db.execSQL("INSERT INTO BloodGlucose VALUES ('2015-01-02 00:00:00', 123);");
+		db.execSQL("INSERT INTO BloodGlucose VALUES (datetime('now'), 234);");
+		showMessage("Success", "Records Inserted!");
+	}
+	
+	public void showMessage(String title,String message)
+	{
+		Builder builder=new Builder(this);
+		builder.setCancelable(true);
+		builder.setTitle(title);
+		builder.setMessage(message);
+		builder.show();
+	}
+	
+	protected void onUpdate(Bundle savedInstanceState) {
+		// TODO: Add code for updating database. 
 	}
 
 	@Override

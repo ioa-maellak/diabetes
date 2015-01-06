05d696af8ae546c17b948a8f8d5e09d54e3ba028
pmanousis <pmanousi@cs.uoi.gr>
2015-01-06 14:18:15 +0200

    Finished with the help of SQL the glucose profile of the patient. It could become a little better (display all the hours) but this is not so important. I also removed the "DROP TABLE" statements, so as to have values in order to see them in the plot.  It's quite easy to make another plot for the minimum and maximum values of the average day.

--- gen/com/example/diabetes/R.java
+++ gen/com/example/diabetes/R.java
@@ -35,7 +35,7 @@ public final class R {
         public static final int action_settings=0x7f080008;
         public static final int buttonBloodGlucoseStats=0x7f080004;
         public static final int buttonBloodMeasurements=0x7f080000;
-        public static final int buttonClosePlot=0x7f080003;
+        public static final int buttonClose=0x7f080003;
         public static final int buttonNutritionInformation=0x7f080001;
         public static final int buttonWorkoutInformation=0x7f080002;
         public static final int datetime=0x7f080006;
@@ -51,17 +51,16 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int blood_glucose_stats=0x7f050008;
-        public static final int blood_measurements=0x7f050003;
-        public static final int close=0x7f050006;
-        public static final int data_of_a_blood_glucose_measurement_=0x7f050007;
-        public static final int display_charts_of_measurements_=0x7f050009;
-        public static final int hello_world=0x7f050001;
-        public static final int nutrition_information=0x7f050004;
-        public static final int title_activity_plot=0x7f05000a;
-        public static final int workout_information=0x7f050005;
+        public static final int blood_glucose_stats=0x7f050007;
+        public static final int blood_measurements=0x7f050002;
+        public static final int close=0x7f050005;
+        public static final int data_of_a_blood_glucose_measurement_=0x7f050006;
+        public static final int display_charts_of_measurements_=0x7f050008;
+        public static final int nutrition_information=0x7f050003;
+        public static final int title_activity_plot=0x7f050009;
+        public static final int workout_information=0x7f050004;
     }
     public static final class style {
         /** 

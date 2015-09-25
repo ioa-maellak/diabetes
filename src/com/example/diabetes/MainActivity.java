d48aba9ce4d9c70cce352ca8d46e04e1c69311ef
Thanos Pappas <thanospappas0@gmail.com>
2015-09-25 16:37:19 +0300

    Workout activity aded

--- src/com/example/diabetes/MainActivity.java
+++ src/com/example/diabetes/MainActivity.java
@@ -99,7 +99,8 @@ public class MainActivity extends Activity {
 		buttonWorkoutInformation.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				showMessage("Success", "Workout info clicked!");	// TODO
+                Intent intent = new Intent(MainActivity.this, WorkoutActivity.class);
+                startActivity(intent);
 			}
 		});
 		
